f52466ab68a6012a7a76c807b1c96fdd3601bfa8
Jey-Vaninah <hei.vaninah@gmail.com>
2025-07-01 23:10:35 +0300

    refactor: format code

--- src/main/java/com/my/serverless/endpoint/rest/controller/health/HelloController.java
+++ src/main/java/com/my/serverless/endpoint/rest/controller/health/HelloController.java
@@ -3,16 +3,15 @@ package com.my.serverless.endpoint.rest.controller.health;
 import com.my.serverless.service.HelloService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
 public class HelloController {
-    private final HelloService helloService;
+  private final HelloService helloService;
 
-    @GetMapping("/hello")
-    public String hello() {
-        return helloService.apply();
-    }
+  @GetMapping("/hello")
+  public String hello() {
+    return helloService.apply();
+  }
 }
